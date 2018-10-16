efed38ed721e95e209355e98942be1cd97f82da6
Simbiso MARWISE <smarwise@e5r6p11.wethinkcode.co.za>
2018-10-16 08:43:13 +0200

    homepage

---
+++ homepage.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Camagru</title>
+	<link rel="stylesheet"  href="homepage.css">
+</head>
+<body>
+	<div class="navbar">
+		<a>Home</a>
+		<a>Login</a>
+		<a>Sign Up</a>
+	  </div>
+	  <br />
+	</body>
+</html>
+<?php
+?>
