74674a1b21cf3d863b956c1fd3ce906c767b8407
Simbiso MARWISE <smarwise@e5r6p11.wethinkcode.co.za>
2018-10-31 10:40:22 +0200

    tired😔

--- config/setup.php
+++ config/setup.php
@@ -7,8 +7,6 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->query("USE ".$dbname);
 
 	$table = "users";
-	$columns = "id int PRIMARY KEY AUTO_INCREMENT NOT NULL, email varchar(255) NOT NULL,  username varchar(255) NOT NULL,
-		passwd varchar(255) NOT NULL";
 	$statement = "CREATE TABLE IF NOT EXISTS `$dbname`.`$table` (
 		id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
 		email varchar(255) NOT NULL, 
